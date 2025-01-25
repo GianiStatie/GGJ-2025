cf4db6c2edc65642afc178227db556fa7f3c3c19
GianiStatie <gianistatie@gmail.com>
2025-01-25 21:28:18 +0200

    player go slow

--- main.gd
+++ main.gd
@@ -19,10 +19,6 @@ func _on_map_move_other_map(not_this_map: Variant, direction: Vector2) -> void:
 	var position_shift = Vector2(not_this_map.size.x, 0) * direction
 	for map in maps:
 		if map != not_this_map:
-			print(Vector2(not_this_map.size.x, 0) * direction)
-			print("not this map: ", not_this_map.name, " ", not_this_map.position)
-			print("this map: ", map.name, " ", map.position, " ", not_this_map.position + position_shift)
-			print('###')
 			map.position = not_this_map.position + position_shift
 			break
 	
