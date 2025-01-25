e34f97ca22b632d5ac33f2d383aeff817be40b40
GianiStatie <gianistatie@gmail.com>
2025-01-25 15:18:31 +0200

    added code for tilemaps

---
+++ map/map.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var map_txt = """
+1111
+1001
+1001
+1111
+"""
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
