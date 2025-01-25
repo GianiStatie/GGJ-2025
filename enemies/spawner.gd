1614c0d632a04885074fbd6db6f468b5571eee60
Paul Parau <paul.parau@live.com>
2025-01-25 21:15:30 +0200

    Enemy scaling

--- enemies/spawner.gd
+++ enemies/spawner.gd
@@ -2,6 +2,9 @@ extends Node
 
 var enemy_scene = preload("res://enemies/enemy.tscn")
 var enemies: Array
+var startTime = Time.get_ticks_msec()
+var difficultyJumpSeconds = 20
+var difficultySpikeSeconds = 60
 
 func _on_enemy_move_timer_timeout() -> void:
 	var player = get_tree().get_nodes_in_group("player_group")[0]
@@ -31,15 +34,29 @@ func _on_enemy_timer_timeout() -> void:
 	if $RayCast2D.get_collider() != player:
 		_on_enemy_timer_timeout()
 		return
-	
+
 	var enemy : Node2D = enemy_scene.instantiate()
 	enemy._set_global_position(globalEnemyPosition)
 	enemy._follow_target(globalPlayerPosition)
+	_set_difficulty(enemy)
 	
 	add_child(enemy)
 	enemies.append(enemy)
 	GameState.enemy_spawns += 1
-
+	
+func _set_difficulty(enemy: Node2D) -> void:
+	var currentTime = Time.get_ticks_msec()
+	var elapsedSeconds = (currentTime - startTime) / 1000
+	
+	var difficultyMultiplier = floor(elapsedSeconds / difficultyJumpSeconds) * 0.1 + 1
+	if elapsedSeconds >= difficultySpikeSeconds && elapsedSeconds % difficultySpikeSeconds < difficultyJumpSeconds:
+		difficultyMultiplier = difficultyMultiplier * 1.5
+	
+	print("Difficulty multiplier: " + str(difficultyMultiplier))
+	
+	enemy.max_health = enemy.max_health * difficultyMultiplier
+	enemy.damage = enemy.damage * difficultyMultiplier
+		
 func _get_enemy_spawn_position() -> Vector2:
 	var screenRect = get_viewport().size
 	var side = randi_range(0, 3)
