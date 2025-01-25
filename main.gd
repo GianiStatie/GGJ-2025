c1a2e276dcd0eb9bd24f01d49809b8864baed1f4
gotero <sorin_adorean@yahoo.com>
2025-01-25 10:21:52 -0800

    reapply settings

--- main.gd
+++ main.gd
@@ -13,6 +13,7 @@ func _process(delta: float) -> void:
 	%SpawnedCount.text = str(GameState.enemy_spawns)
 
 func _on_map_move_other_map(not_this_map: Variant, direction: Vector2) -> void:
+	print(direction)
 	var new_position = Vector2(not_this_map.size.x, 0) * direction + not_this_map.position
 	for map in maps:
 		if map != not_this_map:
