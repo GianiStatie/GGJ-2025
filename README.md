a19e887b3d6385f3431d7145a01d6cd856844411
GianiStatie <gianistatie@gmail.com>
2025-01-26 10:45:32 +0200

    fixed map chunk bug

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Asset pack: 
 - Sprites: [16x16 DungeonTileset II](https://0x72.itch.io/dungeontileset-ii)
 - SFX: [8-bit / 16-bit Sound Effects (x25) Pack](https://jdwasabi.itch.io/8-bit-16-bit-sound-effects-pack)
+- Music: [FREE Chiptune Music Loops](https://tallbeard.itch.io/three-red-hearts-prepare-to-dev)
--- map/map.gd
+++ map/map.gd
@@ -8,8 +8,11 @@ func _ready() -> void:
 	size = $Floor.get_used_rect().size * $Floor.tile_set.tile_size
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_left_area_body_entered(body: Node2D) -> void:
+	var direction = Vector2.LEFT
+	move_other_map.emit(self, direction)
+
+
+func _on_right_area_body_entered(body: Node2D) -> void:
 	var direction = Vector2.RIGHT
-	if body.velocity.x < 0:
-		direction = Vector2.LEFT
 	move_other_map.emit(self, direction)
